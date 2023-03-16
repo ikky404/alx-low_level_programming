1553d622bc3128c22537b0c495a65a5c944df5bb
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-16 16:32:30 -0700

    editing 4th file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
-
+/*
+ *
+ */
 int main() {
-	puts"\"Programming is like building a multilingual puzzle\n";
+	puts"(\"Programming is like building a multilingual puzzle\n");
 	return 0;
 }
