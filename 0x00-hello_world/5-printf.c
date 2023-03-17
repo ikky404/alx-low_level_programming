d5873adaf24330bed59f61a747eba54da181b9ba
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-16 17:10:16 -0700

    adding 5th file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main entry
+ *main -entry
  *
- *return 0
+ *return: 0
  */
-int main(void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
