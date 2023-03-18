3dfccbdcb099023b2e96526cb937934f086a94f0
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-18 07:46:08 -0700

    adding 7th file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Description: print the lowercase alphabet in reverse
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a');
+	{
+		putchar(ch);
+		--ch;
+	}
+	putchar('\n');
+	return (0);
+}
+
