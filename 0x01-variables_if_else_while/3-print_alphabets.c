b3876b597370474c627351685af30a32c21eb288
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-18 04:53:42 -0700

    adding the 3rd file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - start point
+ *
+ * Description: print letters in lower case then upper case
+ *
+ * return: always = 0
+ */
+
+int main (void)
+{
+	char ch = 'a';
+	char CH = 'A';
+	/*Print lower case letters*/
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	/*print upper case letters*/
+
+	while (ch <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar ("\n");
+	return (0);
+
+}
