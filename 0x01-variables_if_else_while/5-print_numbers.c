d39fc9f68d4bf9d1824654e2a4eafe35325cdc83
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-18 07:12:06 -0700

    adding 5th file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description:  prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num = 0;
+	while (num <= 9)
+	{
+		printf("%i", num);
+		num++;
+	}
+	printf('\n');
+	return (0);
+}
