512e76a533945c108d2ed2386b6e9e35964169a8
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-18 11:39:17 -0700

    ignore

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,13 @@
+:include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *description: print all possible combinations of single-digit numbers
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	
