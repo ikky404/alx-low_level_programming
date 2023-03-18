8c5fe69d65e313301830c64cb99b9a626e38d9f9
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-18 11:46:02 -0700

    adding 9th file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,4 @@
-:include <stdio.h>
+#include <stdio.h>
 
 /**
  *main - Entry point
@@ -10,4 +10,19 @@
 
 int main(void)
 {
+	int num =0;
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
 	
+	return (0);
+}
