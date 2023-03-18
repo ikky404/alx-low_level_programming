3aa9fe980996d71b2fdb0ebf3fc1e4620aa74dae
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-18 06:51:36 -0700

    adding 4th file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print lowercase letters except q and e
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <='z');
+	{
+		if (ch == 'e' || ch == 'q')ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
