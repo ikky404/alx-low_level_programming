9b2decc7305b18be13583ac587c1614ac68d0fbe
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-20 14:40:50 -0700

    The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+ *main - entry point
+ *
+ *description:
+ *
+ *Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int num, NUM;
+
+	for (num = 0; num < 9; num++)
+	{
+		for (NUM = num + 1; NUM < 10; NUM++)
+		{
+
+			putchar((num % 10) + '0');
+			putchar((NUM % 10) + '0');
+
+			if (num == 0 && NUM == 0)
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
