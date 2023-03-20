82fc9bde388cebdeee9c4e40e84be55a5196ca50
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-20 13:32:52 -0700

    chage1

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,12 +11,23 @@
 int main(void)
 {
 	int num = 0;
+	char ch = 'a';
 
-	while (num <= 10)
+	while (num <= 9)
+
 	{
 		putchar(num + '0');
 		num++;
 	}
+
+
+ 	 while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
 	putchar('\n');
+
 	return (0);
 }
+
