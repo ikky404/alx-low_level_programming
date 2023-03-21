efd156e998e504b45283e3b0a5e2836f97c3bd0f
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-21 16:46:19 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *jack_bauer - prints every minute of the day from 00:00 to 23:59
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	hour = 0;
+
+	while (hour < 24)
+	{
+		minute = 0;
+		while (minute < 60)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+			hour++;
+		}
+		minute++;
+	}
+}
