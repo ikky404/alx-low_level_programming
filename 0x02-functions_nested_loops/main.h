9fa5ae539360e5540961c914dc5be9a78d981f8c
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-21 07:51:19 -0700

    edit main file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef main_h
+#define main_h
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -9,4 +11,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
+#endif
