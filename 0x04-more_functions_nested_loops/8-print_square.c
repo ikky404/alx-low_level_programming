2663abe2f11839e41573389d6136a70402a30074
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-23 21:10:36 -0700

    Error spoted

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,13 +10,21 @@
 
 void print_square(int size)
 {
-	int x, y;
-
-	for (x = 1; x <= size; ++x)
+	if (size <= 0)
 	{
-		for (y = 1; y <= size; ++y)
-			_putchar('#');
 		_putchar('\n');
+	} else
+	{
+		int x, y;
+
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
+
