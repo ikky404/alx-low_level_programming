a74c1a4a945c80a16c68c0fedbe5bc0084680664
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-28 15:58:31 -0700

    sacrifice the numbers to save the people

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,22 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ * _atoi - convert a string to an integer
+ * @s: pointer to string
+ * Return: the int value of str
  */
 int _atoi(char *s)
 {
+	int str = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+		str *= -1;
+		else if (*s >= '0' && *s <= '9')
+		num = num * 10 + (*s - '0');
+		else if (num > 0)
+		break;
+	} while (*s++);
+
+	return (num * str);
+}
