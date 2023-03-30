2f278efed2a16e970f16b31b4b4c2def463ad49c
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-30 15:27:11 -0700

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,34 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
+ * cap_string - capitalizes all words of a string
  *
- * Return: Always 0.
+ * @str: string
+ *
+ * Return: value of str
  */
-int main(void)
+char *cap_string(char *str)
 {
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
+	int x, y;
+	char s[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
+	x = 0;
+
+	while (str[x] != 0)
+	{
+		if (x == 0 && str[x] >= 97 && str[x] <= 122)
+		{
+			str[x] = str[x] - 32;
+		}
+		y = 0;
+		while (s[y] != 0)
+		{
+			if (s[y] == str[x] && (str[x + 1] >= 97 && str[x + 1] <= 122))
+			{
+				str[x + 1] = str[x + 1] - 32;
+			}
+			y++;
+		}
+		x++;
+	}
+	return (str);
 }
