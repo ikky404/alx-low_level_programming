1f6ca7bc913611b6db5328e48ae26c8ae36d53b9
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-30 06:27:22 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,30 @@
 #include "main.h"
 /**
+ * _strncat - concatenates two strings
  *
+ *@dest: destination
+ *@src: source
+ *@n: integer
  *
- *
- *
- *
+ * Return: retun value of dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int x, y, z;
+
 	x = y = z = 0;
 
+	for (; dest[z];)
+	{
+		z++;
+	}
+
+	while (y < n && src[y])
+	{
+		dest[z + x] = src[y];
+		x++;
+		y++;
+	}
+
+	return (dest);
+}
