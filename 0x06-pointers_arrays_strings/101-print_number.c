6ba7f22a7768391530b076b1e7bdd950898faacf
ikky404 <jalil.ikram.jali@gmail.com>
2023-03-31 09:54:57 -0700

    print an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,7 +13,9 @@ void print_number(int n)
 		_putchar('-');
 		digit = -digit;
 	}
+
 	if ((digit / 10) > 0)
 		print_number(digit / 10);
-	_putchar((digit % 10) + 0);
+
+	_putchar((digit % 10) + '0');
 }
