55ce690a0fcaee90191b2a9692d12ef6832be17c
ikky404 <jalil.ikram.jali@gmail.com>
2023-04-03 18:58:04 -0700

    first file to e commited

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_memset - function that fills memory with a constant byte
+ *@s: memory area
+ *@b: constant bytes
+ *@n: number of bytes
+ *
+ *Return: value of s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
