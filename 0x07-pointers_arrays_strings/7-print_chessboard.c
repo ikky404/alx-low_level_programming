980e88e541947fa5cd9a6161a10b4d21c2533623
ikky404 <jalil.ikram.jali@gmail.com>
2023-04-04 16:35:19 -0700

    prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,3 +7,14 @@
  *Return: void
  */
 void print_chessboard(char (*a)[8])
+{
+	int x, y = 0;
+
+	for (x = 0; a[x][7]; x++)
+	{
+		for (y = 0; y < 8; y++)
+			_putchar(a[x][y]);
+		_putchar('\n');
+	}
+
+}
