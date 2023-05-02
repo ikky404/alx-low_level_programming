571bbdc0dd2b6123ec470da20a490fbf7d49d5f0
ikky404 <jalil.ikram.jali@gmail.com>
2023-05-02 15:19:49 -0700

    corrected

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void before_exmain(void);
+
+int main(void)
+{
+	before_exmain();
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
