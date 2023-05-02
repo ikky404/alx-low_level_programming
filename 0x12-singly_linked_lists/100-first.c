e5e47ff35480e4f09b0980708a5efd279340da7d
ikky404 <jalil.ikram.jali@gmail.com>
2023-05-02 15:08:22 -0700

    test

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+#include "stdio.h"
 /**
  * before_exmain - function executed before main
  */
@@ -6,4 +6,14 @@ void before_exmain(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
+}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
 }
