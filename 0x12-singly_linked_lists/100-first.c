66aeecc248eab057cd4238d62add84adcd13527b
ikky404 <jalil.ikram.jali@gmail.com>
2023-05-02 14:56:24 -0700

    test

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * before_exmain - function executed before main
+ */
+void before_exmain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
