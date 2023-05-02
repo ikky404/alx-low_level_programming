7f4b6eabd57e369a4bc563ad15ed684e748ca927
ikky404 <jalil.ikram.jali@gmail.com>
2023-05-02 14:33:55 -0700

    List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
- * list_len - a func that returns the number of elements in a linked list_t list
+ * list_len - a func that returns the number of elements
+ * in a linked list_t list
  * @h: data type pointer of struct
  *
  * Return: elements of the str i
