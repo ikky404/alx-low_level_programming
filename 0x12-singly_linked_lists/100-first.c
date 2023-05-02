4ac164c4007a36b8c36076173ed7b6f68179cf90
ikky404 <jalil.ikram.jali@gmail.com>
2023-05-02 15:09:10 -0700

    test

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,13 +7,3 @@ void before_exmain(void)
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
-}
