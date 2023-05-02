ae8b7c1c7867cd4bcf035723e83133458843d561
ikky404 <jalil.ikram.jali@gmail.com>
2023-05-02 15:30:28 -0700

    test

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -9,6 +9,7 @@ void before_exmain(void);
 int main(void)
 {
 	before_exmain();
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	printf("(A tortoise, having pretty good sense of a hare's nature,
+		challenges one to a race.)\n");
 	return (0);
 }
