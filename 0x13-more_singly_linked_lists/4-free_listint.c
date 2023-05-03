6de063b650271836a35375b31d8bc7213b9514dd
ikky404 <jalil.ikram.jali@gmail.com>
2023-05-02 17:04:15 -0700

    free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 /**
- * free_listint -  a function that frees a listint_t list
- * @head: data type pointer the head/next node
- */
+* free_listint - a function that frees a listint_t list
+* @head: ata type pointer the head
+*/
 void free_listint(listint_t *head)
 {
-	while (head != NULL)
-	{
-		free(head);
-		head = head->next;
-	}
+
+	if (!head)
+		return;
+	free_listint(head->next);
+	free(head);
 }
